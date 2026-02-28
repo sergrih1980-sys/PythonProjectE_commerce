f43b0db1fc8c8a6078c73a84f908d4f9ded0e92d
sergrih1980-sys <sergrih1980@gmail.com>
2026-02-28 22:03:57 +0300

    написание кода

---
+++ src/product.py
@@ -0,0 +1,15 @@
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+    product_count = 0
+
+    def __init__(self, name, price, quantity, description):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.description = description
+        Product.product_count += 1
