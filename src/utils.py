f288d273f4cb3db25a4a3125a3b740c96e99c3e5
sergrih1980-sys <sergrih1980@gmail.com>
2026-02-28 20:09:52 +0300

    написание кода

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ class Category:
     description: str
     products: list
     category_count = 0
-    product_count = 0
+
 
     def __init__(self, name, description, products ):
         self.name = name
--- tests/conftest.py
+++ tests/conftest.py
@@ -13,9 +13,23 @@ def products():
     )
 
 @pytest.fixture
-def categories():
-    return Category(
-
-
-
+def sample_product():
+    return Product(
+        name="55\" QLED 4K",
+        price=123000.0,
+        quantity=7,
+        description="Фоновая подсветка"
     )
+
+@pytest.fixture
+def sample_category():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        product3=Product(
+            name="Xiaomi Redmi Note 11",
+            price=31000.0,
+            quantity=14,
+            description="1024GB, Синий"
+
+        ))
