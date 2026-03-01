844e8cd4d392e95b326eee9689f5f6e4b4fa6b5d
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-01 17:57:43 +0300

    написание кода

--- src/product.py
+++ src/product.py
@@ -5,11 +5,11 @@ class Product:
     description: str
     price: float
     quantity: int
-    product_count = 0
+
 
     def __init__(self, name, price, quantity, description):
         self.name = name
         self.price = price
         self.quantity = quantity
         self.description = description
-        Product.product_count += 1
+
