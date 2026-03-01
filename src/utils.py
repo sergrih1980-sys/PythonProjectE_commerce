8e064c8843a09ec383273b40f30c6753e92eb9fb
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-01 18:14:50 +0300

    написание кода

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,6 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
