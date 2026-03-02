c9a74d3787fe99ac3f42246f4673fc22e6b25588
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-02 22:03:20 +0300

    написание кода

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from src.product import Product
 
 
 class Category:
@@ -10,6 +11,29 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        result = []
+        for product in self.__products:
+           result.append(f"{product.name},{product.price}руб. Остаток: {product.quantity}")
+        return result
+
+
+
+
+
+
+
+
+
+
+
