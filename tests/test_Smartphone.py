8b736a882c422ea62cb73b2ac7dce73255799ba0
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-14 18:50:30 +0300

    Исправление ошибок

--- tests/test_Smartphone.py
+++ tests/test_Smartphone.py
@@ -1,15 +1,15 @@
 import pytest
 
 
-def test_smartphone_init(product_smartphone):
-    assert product_smartphone.name == "Xiaomi Redmi Note 11"
-    assert product_smartphone.description == "1024GB, Синий"
-    assert product_smartphone.price == 31000.0
-    assert product_smartphone.quantity == 14
-    assert product_smartphone.efficiency == 90.3
-    assert product_smartphone.model == "Note 11"
-    assert product_smartphone.memory == 1024
-    assert product_smartphone.color == "Синий"
+def test_smartphone_init(product_smartphone1):
+    assert product_smartphone1.name == "Xiaomi Redmi Note 11"
+    assert product_smartphone1.description == "1024GB, Синий"
+    assert product_smartphone1.price == 31000.0
+    assert product_smartphone1.quantity == 14
+    assert product_smartphone1.efficiency == 90.3
+    assert product_smartphone1.model == "Note 11"
+    assert product_smartphone1.memory == 1024
+    assert product_smartphone1.color == "Синий"
 
 
 def test_smartphone_add_product(product_smartphone, product_smartphone2):
@@ -18,8 +18,8 @@ def test_smartphone_add_product(product_smartphone, product_smartphone2):
     assert result == 180000.0 * 5 + 210000.0 * 8
 
 
-def test_smartphone_add_error_product(product_smartphone,
+def test_smartphone_add_error_product(product_smartphone1,
                                       product_smartphone2):
     with pytest.raises(TypeError, match="Нельзя складывать с объектом, "
                                         "не являющимся продуктом"):
-        product_smartphone + 1
+        product_smartphone1 + 1
