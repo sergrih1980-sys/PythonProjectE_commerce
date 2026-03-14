5ec8fd856e4b19eab0d66a6d87894629c0791f8d
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-14 18:41:20 +0300

    Исправление ошибок

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,7 +81,7 @@ def category_with_products(valid_product):
 
 
 @pytest.fixture
-def product_smartphone():
+def product_smartphone1():
     return Smartphone("Samsung Galaxy S23 Ultra",
     "256GB, Серый цвет, 200MP камера",
     180000.0,
