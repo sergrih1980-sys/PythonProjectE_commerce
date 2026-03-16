eb40476c61b7c727aab14e10bce0ed4e37ace673
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-16 19:50:20 +0300

    Исправление ошибок

--- tests/test_print_mixin.py
+++ tests/test_print_mixin.py
@@ -4,8 +4,27 @@ from src.LawnGrass import LawnGrass
 
 def test_print_mixin(capsys):
     Product("Test Product", "Test description", 100, 10)
-    message, _ = capsys.readouterr()
-    assert message.out.strip() ==  "Product('Test Product', 'Test description', 100, 10)"
+    out, err = capsys.readouterr()
+    expected_product = "Product('Test Product', 'Test description', 100, 10)"
+    assert out.strip() == expected_product
 
 
+    capsys.readouterr()
+
+    Smartphone(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14,
+        90.3,
+        "Note 11",
+        1024,
+        "Синий"
+    )
+    out, err = capsys.readouterr()
+    expected_smartphone = (
+        "Smartphone('Xiaomi Redmi Note 11', '1024GB, Синий', 31000.0, "
+        "14, 90.3, 'Note 11', 1024, 'Синий')"
+    )
+    assert out.strip() == expected_smartphone
 
