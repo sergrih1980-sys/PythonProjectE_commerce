3faeccd5424cedb52490cf9a12dd472d2b0b7053
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-16 19:53:47 +0300

    Исправление ошибок

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -1,6 +1,7 @@
 class PrintMixin:
     def __init__(self, *args, **kwargs) -> None:
         print(repr(self))
+        super().__init__(*args, **kwargs)
 
     def __repr__(self):
         return (f"{self.__class__.__name__}('{self.name}',"
