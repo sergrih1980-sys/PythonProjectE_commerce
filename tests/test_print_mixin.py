5d4cc29dae2b198d5ebdcaa76ea0a78f38bd185a
sergrih1980-sys <sergrih1980@gmail.com>
2026-03-16 19:47:19 +0300

    Исправление ошибок

--- tests/test_print_mixin.py
+++ tests/test_print_mixin.py
@@ -5,7 +5,7 @@ from src.LawnGrass import LawnGrass
 def test_print_mixin(capsys):
     Product("Test Product", "Test description", 100, 10)
     message, _ = capsys.readouterr()
-    assert message.out.strip() ==  "Product('Test Product', 'Test description', '100', 10)"
+    assert message.out.strip() ==  "Product('Test Product', 'Test description', 100, 10)"
 
 
 
